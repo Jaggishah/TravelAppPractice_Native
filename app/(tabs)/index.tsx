a666bd4760e93d6bb0d07c9048e085b2babc9390
Jaggishah <jugdeepshah@gmail.com>
2024-06-11 23:39:58 -0700

    added first screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,88 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity,Image, TextInput } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import Color from '@/constants/Color'
+import { useHeaderHeight } from '@react-navigation/elements';
+import CategoryButton from '@/components/CategoryButton'
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+const index = () => {
+    const headerHeight = useHeaderHeight();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <>
+    <Stack.Screen options={{
+        headerTransparent : true,
+        headerTitle : "",
+        headerLeft:() => (
+            <TouchableOpacity onPress={() => {}} style={{marginLeft:20}}>
+                <Image source ={{ uri : "https://xsgames.co/randomusers/avatar.php?g=female"}} 
+                    style={{width:40,height:40,borderRadius:10}}
+                />
+            </TouchableOpacity>
+        ),
+        headerRight:() => (
+            <TouchableOpacity onPress={() => {}} style={{
+                marginRight:20,
+                backgroundColor:Color.white,
+                padding:10,
+                borderRadius:10,
+                shadowColor: "#171717",
+                shadowOffset: { width : 2, height : 4},
+                shadowOpacity: 0.2,
+                shadowRadius : 3
+                }}>
+                <Ionicons  name='notifications' size={20} color={Color.black}/>
+            </TouchableOpacity>
+        )
+    }}/>
+    <View style={[styles.container , { paddingTop : headerHeight}]}>
+        <Text style={ styles.headingTxt}> Explore the beautiful World</Text>
+        <View style={styles.searchSectionWrapper}>
+            <View style={styles.searchBar}>
+                <Ionicons name='search' size={18} style={{marginRight:5}} color={Color.black}/>
+                <TextInput placeholder='Search...'/>
+            </View>
+            <TouchableOpacity onPress={() => {}} style={styles.filterBtn}>
+                <Ionicons name='options' size={28} color={Color.white}/>
+            </TouchableOpacity>
+        </View>
+        <CategoryButton/>
     </View>
-  );
+    
+    </>
+  )
 }
 
+export default index
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container:{
+    flex : 1,
+      paddingHorizontal: 20,
+      backgroundColor : Color.bgColor,
+    },
+    headingTxt:{
+        fontSize: 28,
+        fontWeight: '800',
+        color: Color.black,
+        marginTop : 10,
+    },
+    searchSectionWrapper:{
+        flexDirection:'row',
+        marginVertical:20,
+    },
+    searchBar:{
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        backgroundColor: Color.white,
+        padding:16,
+        borderRadius:10
+    },
+    filterBtn:{
+        backgroundColor:Color.primaryColor,
+        padding:12,
+        borderRadius:10,
+        marginLeft:20
+    }
 });
---
+++ components/CategoryButton.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CategoryButton() {
+  return (
+    <View>
+      <Text>CategoryButton</Text>
+    </View>
+  )
+}
